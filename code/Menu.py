b6bdfc6caec4210e1989584be07c2cde0de70a13
Laboratorio CPEx <jhon.cleiton@cpexgroup.com.br>
2025-10-08 11:05:56 -0300

    Changing menu

--- code/Menu.py
+++ code/Menu.py
@@ -20,8 +20,8 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, 'Mountain', (COLOR_ORANGE), ((WIN_WIDHT / 2), 70))
-            self.menu_text(50, 'Shooter', (COLOR_ORANGE), ((WIN_WIDHT / 2), 120))
+            self.menu_text(50, 'Mountain', (COLOR_WHITE), ((WIN_WIDHT / 2), 70))
+            self.menu_text(50, 'Shooter', (COLOR_WHITE), ((WIN_WIDHT / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 self.menu_text(20, MENU_OPTION[i], (COLOR_WHITE), ((WIN_WIDHT / 2), 200 + 25 * i))
